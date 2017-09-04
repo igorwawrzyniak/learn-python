866e156bd19e15080b3ed1c39eb51bf71f091a64
Igor Wawrzyniak <igor@wawrzyniak.pro>
2017-09-04 14:31:23 +0200

    Rezygnacja z fileinput, na razie brak obslugi pliku, dodane parsowanie opcji, dziala w Pythonie 2 i 3

--- ppg.py
+++ ppg.py
@@ -1,7 +1,24 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import sys
+import argparse
 
-for line in sys.stdin:
-    print(line.strip())
+def printfile(printname=False,printline=False,filename='stdin'):
+    if printname:
+        print(filename)
+    lineno=1
+    for line in sys.stdin:
+        if printline:
+            sys.stdout.write(str(lineno)+' ')
+            lineno+=1
+        print(line.strip('\n'))
 
+def setargparse():
+    parser=argparse.ArgumentParser(description='Primitive Python Grep.')
+    parser.add_argument('-H','--with-filename', action='store_true', help='Show filename',dest='printname')
+    parser.add_argument('-n', '--line-number', action='store_true', help='Show line number', dest='printline')
+    parser.add_argument('-i','--input-file', action='store', help='Input file (empty for stdin)',dest='filename',default='stdin')
+    return parser.parse_args()
+
+args=setargparse()
+printfile(printname=args.printname,printline=args.printline,filename=args.filename)
